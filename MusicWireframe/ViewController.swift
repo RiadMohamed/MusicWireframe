90270a858eb3006cb3d48394d0a4f6c22d8c86ac
Riad Mohamed <riaduk1@gmail.com>
2019-07-27 22:44:02 +0200

    Added outlets for the view elements.

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var reverseShadow: UIView!
+    @IBOutlet weak var playPauseShadow: UIView!
+    @IBOutlet weak var forwardShadow: UIView!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
