15f8eeb3e863546cd026ba464f7aae2351a76005
Riad Mohamed <riaduk1@gmail.com>
2019-07-28 18:24:10 +0200

    UIViews initialized, added a property to track if there is a song playing or paused.

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isPlaying: Bool = true {
+        didSet {
+            if isPlaying {
+                playPauseButton.setImage(UIImage(named: "pause")!, for: .normal)
+            } else {
+                playPauseButton.setImage(UIImage(named: "play")!, for: .normal)
+            }
+        }
+    }
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var reverseShadow: UIView!
     @IBOutlet weak var playPauseShadow: UIView!
@@ -19,7 +28,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var forwardButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reverseShadow.layer.cornerRadius = 35.0
+        reverseShadow.clipsToBounds = true
+        reverseShadow.alpha = 0
+        
+        playPauseShadow.layer.cornerRadius = 35.0
+        playPauseShadow.clipsToBounds = true
+        playPauseShadow.alpha = 0
+        
+        forwardShadow.layer.cornerRadius = 35.0
+        forwardShadow.clipsToBounds = true
+        forwardShadow.alpha = 0
     }
 
 
